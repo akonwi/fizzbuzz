14c3a4fbda80784bc6c3fc31de1d941b0918a992
akonwi <akonwi@codestream.com>
2018-09-03 17:34:07 -0500

    new file

---
+++ init.js
@@ -0,0 +1,19 @@
+atom.packages.onDidActivateInitialPackages(() => {
+  const gitPlus = atom.packages.getActivePackage("git-plus")
+  if (gitPlus) {
+    const gp = gitPlus.mainModule.provideService()
+    gp.registerCommand(
+      "atom-text-editor",
+      "your-custom-namespace:add-chmod",
+      () => {
+        gp
+          .getRepo() // If there are multiple repos in the project, you will be prompted to select which to use
+          .then(repo => {
+            const activeEditor = atom.workspace.getActiveTextEditor()
+            if (activeEditor)
+              gitPlus.run(repo, `add --chmod=+x ${activeEditor.getPath()}`)
+          })
+      }
+    )
+  }
+})
